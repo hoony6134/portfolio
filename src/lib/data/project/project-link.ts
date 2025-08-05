4a633c277dce0470fa45baf6a88d93fd13d88255
Cyan <hoony6134@gmail.com>
2025-08-05 16:21:14 +0900

    fix: link for NutriCheck intro

--- src/lib/data/project/project-link.ts
+++ src/lib/data/project/project-link.ts
@@ -13,7 +13,7 @@ export const projectLinks: ProjectLink[] = [
   {
     id: 'nutricheck-intro',
     title: 'Intro',
-    url: 'https://viewer.science.go.kr:8080/SynapDocViewServer/viewer/doc.html?key=ffc3d805310a4bf7bd0440a280c3b64e&convType=img&convLocale=ko_KR&contextPath=/SynapDocViewServer',
+    url: 'https://science.go.kr/mps/1075/bbs/424/moveBbsNttDetail.do?nttSn=44406&page=1&searchCnd=aditfield4&aditfield10=&aditfield8=&searchKrwd=%EC%9E%84%EC%A0%95%ED%9B%88',
     icon: IconFileCheck,
   },
 ]
